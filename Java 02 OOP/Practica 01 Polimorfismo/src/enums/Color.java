e1b699b8ef9282e696d291552f1f666f8e6f9349
Fernando Carrillo Castro <fernando17carrillo@hotmail.com>
2016-07-06 12:27:40 -0700

    Changing names on OOP topics
    The topics were previously on spanish...still missing the class name
    though

--- Java 02 OOP/Practica 01 Polimorfismo/src/enums/Color.java
+++
@@ -1,9 +0,0 @@
-package enums;
-
-public enum Color {
-BLANCO("Es blanco"),NEGRO("Es negro"),ROJO("Es rojo");
-String msg;
-private Color(String msg){
-	this.msg=msg;
-}
-}
--- Java 02 OOP/Practica 01 Polimorfismo/src/enums/Sexo.java
+++
@@ -1,9 +0,0 @@
-package enums;
-
-public enum Sexo {
-MASCULINO("Es un hombre"),FEMENINO("Es una mujer");
-String msg;
-private Sexo(String msg){
-	this.msg=msg;
-}
-}
